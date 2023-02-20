f79784aa6ff22a51351c9cd4c12aaae623ec462c
Veeti Oivo <n1oive00@students.oamk.fi>
2023-02-20 20:14:08 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert, Pressable } from 'react-native';
+import NumericInput from 'react-native-numeric-input'
+import { RadioButton, Switch } from "react-native-paper"
+import { useState } from 'react/cjs/react.development';
+import { Styles, SpecialStyles } from "./styles/Styles"
+
 
 export default function App() {
+
+  const [gender, setGender] = useState("male");
+  const [weight, setWeight] = useState(0);
+  const [bottles, setBottles] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [result, setResult] = useState(0);
+
+  function calculate() {
+    let litres = bottles * 0.33
+    let grams = litres * 8 * 4.5
+    let burning = weight / 10
+    let gramsLeft = grams - burning * hours
+
+    if (weight <= 0) {
+      Alert.alert("Incorrect weight")
+      return;
+    }
+
+    if(bottles === 0) {
+      setResult(0)
+    }
+
+    if (gender == "male") {
+      setResult(gramsLeft / (weight * 0.7))
+    }
+    else{
+      setResult(gramsLeft / (weight * 0.6))
+    }
+
+
+  }
+
+
+  const [isSpecial, setSpecial] = useState(false);
+
+  const myStyle = isSpecial ? SpecialStyles : Styles;
+  const info = isSpecial ? "Dark mode on" : "Dark mode off"
+  const selected = isSpecial ? SpecialStyles: Styles;
+
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={myStyle.container}>
+          <Switch
+            value={isSpecial}
+            onValueChange={() => setSpecial(!isSpecial)}
+            thumbColor="#76c2e2"
+            trackColor={{ false: "#99cde3", true: "#ababab" }}
+            style={myStyle.darkmode}
+          >
+          </Switch>
+          <Text>{info}</Text>
+      
+      
+        <Text style={Styles.title}>Alcometer</Text>
+        <Text style={Styles.text}>Weight:</Text>
+        <TextInput
+          keyboardType='number-pad'
+          onChangeText={(n) => setWeight(Number.parseInt(n))}
+          placeholder="Your weight (kg)"
+          style={Styles.textInput}
+        />
+        <View>
+        <Text style={Styles.text}>Bottles</Text>
+        <NumericInput
+          onChange={(b) => setBottles(b)}
+          upDownButtonsBackgroundColor="#7cd0f4"
+          type='up-down'
+          rounded
+          borderColor={"#00b3ff"}
+          minValue={0}
+        />
+        </View>
+        <View>
+        <Text style={Styles.text}>Hours</Text>
+        <NumericInput
+          onChange={(h) => setHours(h)}
+          upDownButtonsBackgroundColor="#7cd0f4"
+          type='up-down'
+          rounded
+          borderColor={"#00b3ff"}
+          minValue={0}
+        />
+        </View>
+        <RadioButton.Group
+          onValueChange={newValue => setGender(newValue)}
+          value={gender}
+        >
+          <View style={Styles.radioStyle}>
+            <RadioButton value="male" />
+            <Text>Male</Text>
+            <RadioButton value="female" />
+            <Text>Female</Text>
+          </View>
+        </RadioButton.Group>
+        <Pressable onPress={calculate} style={Styles.button}>
+          <Text style={Styles.buttonText}>Calculate</Text>
+        </Pressable>
+        <Text style={Styles.result}>{result.toFixed(2)} ‰</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/Styles.js
@@ -0,0 +1,106 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+
+export const Styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#e5e5e5',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 20,
+      marginTop: Constants.statusBarHeight,
+    },
+    text:{
+        padding: 10,
+        margin: 10,
+    },
+    textInput :{
+      borderWidth: 1,
+      padding: 5,
+      textAlign: 'center',
+      backgroundColor: "#fff",
+    },
+    
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+    button: {
+        height: 40,
+        width: 120,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 5,
+        borderWidth: 1,
+        backgroundColor: "#f3a724",
+        margin: 20,
+    },
+    buttonText:{
+        fontSize: 18,
+    },
+    result:{
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 10,
+      margin: 10,
+      borderWidth: 1,
+      fontSize: 20,
+      backgroundColor: "#ffffff"
+    },
+    radioStyle:{
+      flexDirection:"row", 
+      alignItems:"center",
+      margin: 20,
+    },
+    darkmode: {
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+      backgroundColor: '#e5e5e5',
+      marginLeft: 15,
+    },
+    numericButtonColor: {
+      color :"#d8d5d5"
+    },
+
+  });
+
+  export const SpecialStyles = StyleSheet.create({
+    container: {
+      ...Styles.container,
+      backgroundColor: '#7d7d7d',
+    },
+    text:{
+        padding: 10,
+        margin: 10,
+    },
+    textInput :{
+      borderWidth: 1,
+      padding: 5,
+      textAlign: 'center',
+      backgroundColor: "#757575",
+
+    },
+    numeric: {
+        borderRadius: 5,
+        borderWidth: 1,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+    button: {
+        ...Styles.button
+    },
+    darkmode: {
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+      backgroundColor: '#7d7d7d',
+      color: "#ffffff"
+    },
+    result: {
+      ...Styles.result,
+    }
+  });
+
+
+  
